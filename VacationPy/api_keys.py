c6b91226fea33b0687a84e0d2243580aab316ec4
Alex Saunders <alexsaunderscodes@gmail.com>
2021-07-23 15:44:37 -0400

    removed some materials

--- VacationPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
-
-# Google API Key
-g_key = "AIzaSyBWLrrNWgdCkQg0Xzz4g0AI2ai7L6i5hU8"
--- WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "5e81c914c743e5df34932e554f6bfd5f"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
